d912763a49747a520ebc35d32be7a1b310c2c5ed
toby <thinktkj@gmail.com>
2019-10-10 16:40:52 +0800

    配置优化

--- src/main/java/vip/toby/rpc/server/RabbitMQConfiguration.java
+++ src/main/java/vip/toby/rpc/server/RabbitMQConfiguration.java
@@ -1,12 +1,10 @@
 package vip.toby.rpc.server;
 
-import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @ConditionalOnProperty(prefix = "simple-rpc", name = "mode", havingValue = "server")
