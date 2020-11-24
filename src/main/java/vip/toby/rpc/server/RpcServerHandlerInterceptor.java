1a167c31fe3c1c1681927d50b824b387c48b0d90
toby <thinktkj@gmail.com>
2020-11-25 05:10:46 +0800

    1.3.7版本

--- src/main/java/vip/toby/rpc/server/RpcServerHandlerInterceptor.java
+++ src/main/java/vip/toby/rpc/server/RpcServerHandlerInterceptor.java
@@ -9,11 +9,18 @@ public interface RpcServerHandlerInterceptor {
 
     /**
      * 用来做幂等处理, 仅针对 RpcType.SYNC
+     *
+     * @param correlationId 消息ID
+     * @return 是否重复
      */
     boolean rpcDuplicateHandle(String correlationId);
 
     /**
      * 通过调用参数做重复调用检测
+     *
+     * @param method 方法
+     * @param data 参数
+     * @return 是否重复
      */
     boolean duplicateHandle(String method, Object data);
 
